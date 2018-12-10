5e9f507d098878b691fc47ff53f6c1560d011fc7
Oscar <oscargonzalezarbesu@gmail.com>
2018-12-10 09:05:11 +0100

    Leyendo datos

--- src/clases/Humano.java
+++ src/clases/Humano.java
@@ -1,5 +1,10 @@
 package clases;
 
+import java.util.Scanner;
+
 public class Humano {
-	
+	public static void main(String[] args) {
+		Scanner tc =new Scanner(System.in);
+		System.out.println();
+	}
 }
