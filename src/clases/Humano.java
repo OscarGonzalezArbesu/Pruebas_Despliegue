830a189ff60d9c5db66fce45af24f6a143a5f717
Oscar <oscargonzalezarbesu@gmail.com>
2018-12-10 09:01:03 +0100

    En rama de ficheros

--- src/clases/Humano.java
+++ src/clases/Humano.java
@@ -1,5 +1,19 @@
 package clases;
 
+import java.io.File;
+
 public class Humano {
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 	
+	private void initTodo() {
+		File fichero=new File("hola.txt");
+	}
 }
